8ad4929e614b36165cf2549367aa1cfb2b294705
Simone Romiti <sr1992ei@gmail.com>
2020-11-25 18:56:13 +0100

    Dropdown anche con click

--- js/script.js
+++ js/script.js
@@ -7,13 +7,24 @@ $(document).ready(
         function(){
           var dropdown = $(this).children(".box-container");
           dropdown.fadeIn(400);
-      });
+
+          $(this).click(function(){
+            if(dropdown.css("display") == "block"){
+              dropdown.fadeOut(400);
+            } else if(dropdown.css("display") == "none"){
+              dropdown.fadeIn(400);
+              }
+            }
+          )
+        }
+      );
 
       $(this).mouseleave(
         function(){
           var dropdown = $(this).children(".box-container");
           dropdown.fadeOut(400);
-      });
+        }
+      );
 
     }
   );
