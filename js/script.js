e5b926fbde10d5183f0824c9f1aa975d33b2ff4b
Simone Romiti <sr1992ei@gmail.com>
2020-11-25 19:45:05 +0100

    Esercizio completo

--- js/script.js
+++ js/script.js
@@ -2,32 +2,29 @@ $(document).ready(
   function(){
 
 
-    $(".dropmenu").each(function(){
-      $(this).mouseenter(
-        function(){
-          var dropdown = $(this).children(".box-container");
-          dropdown.fadeIn(400);
+    $(".dropmenu").each(
+      function(){
+        $(this).mouseenter(
+          function(){
+            var dropdown = $(this).children(".box-container");
+            dropdown.fadeIn(400);
+          }
+        );
 
-          $(this).click(function(){
-            if(dropdown.css("display") == "block"){
-              dropdown.fadeOut(400);
-            } else if(dropdown.css("display") == "none"){
-              dropdown.fadeIn(400);
-              }
-            }
-          )
-        }
-      );
+        $(this).click(
+          function(){
+            var dropdown = $(this).children(".box-container");
+            dropdown.fadeToggle(400);
+          }
+        );
 
-      $(this).mouseleave(
-        function(){
-          var dropdown = $(this).children(".box-container");
-          dropdown.fadeOut(400);
-        }
-      );
-
-    }
-  );
-
+        $(this).mouseleave(
+          function(){
+            var dropdown = $(this).children(".box-container");
+            dropdown.fadeOut(400);
+          }
+        );
+      }
+    );
   }
 );
