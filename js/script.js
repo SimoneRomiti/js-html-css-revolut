1d58d04980805f20f70004ad60070d23a7b6d10a
Simone Romiti <sr1992ei@gmail.com>
2020-11-25 18:35:49 +0100

    Dropdown mousenter mouseleave

--- js/script.js
+++ js/script.js
@@ -2,9 +2,19 @@ $(document).ready(
   function(){
 
 
-    $("#abbonamenti").mouseenter(function() {
-       var dropdown = $(this).children(".box-container");
-       dropdown.fadeIn("slow");
+    $(".dropmenu").each(function(){
+      $(this).mouseenter(
+        function(){
+          var dropdown = $(this).children(".box-container");
+          dropdown.fadeIn(400);
+      });
+
+      $(this).mouseleave(
+        function(){
+          var dropdown = $(this).children(".box-container");
+          dropdown.fadeOut(400);
+      });
+
     }
   );
 
